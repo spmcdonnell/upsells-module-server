0d79382b5df9ec21bd11776d8904e3a8207ba40a
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2021-09-08 22:08:00 -0400

    Troubleshooting db connection for heroku

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,10 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized },
+        },
         useNullAsDefault: true,
         migrations: {
             directory: './database/migrations'
